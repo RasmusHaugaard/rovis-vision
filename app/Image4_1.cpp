25f7236986acbdbbbeb323ec2c607d1bd2cf368a
Unknown <Frederikk@live.dk>
2018-10-05 11:54:45 +0200

    4_1 done

--- app/Image4_1.cpp
+++ app/Image4_1.cpp
@@ -5,50 +5,41 @@
 
 int main()
 {
+    const float scale = 0.3;
     cv::Mat src = cv::imread("../images/Image4_1.png", cv::IMREAD_GRAYSCALE);
 
     vis::show("src", src, 0.3);
 
-    cv::Mat padded;                            //expand input image to optimal size
-    int m = cv::getOptimalDFTSize( src.rows );
-    int n = cv::getOptimalDFTSize( src.cols ); // on the border add zero pixels
-    copyMakeBorder(src, padded, 0, m - src.rows, 0, n - src.cols, cv::BORDER_CONSTANT, cv::Scalar::all(0));
+    cv::Mat fft2d = vis::fft2d(src);
+    vis::showfft2dMagnitude("src freq mag", fft2d, scale / 2);
 
-    cv::Mat planes[] = {cv::Mat_<float>(padded), cv::Mat::zeros(padded.size(), CV_32F)};
-    cv::Mat complexI;
-    merge(planes, 2, complexI);
+    cv::Point centerPoint(fft2d.cols / 2, fft2d.rows / 2);
+    double r = fft2d.cols / 4.0 *1.1;
 
-    dft(complexI, complexI);
+    double angle1 = (0.75*M_PI);
+    double angle2 = (1.75*M_PI);
+    int x = int(round(cos(angle1) * r + centerPoint.x));
+    int y = int(round(sin(angle1) * r + centerPoint.y));
+    cv::circle(fft2d, cv::Point(x, y), 35, cv::Scalar(0), -1);
+    x = int(round(cos(angle2) * r + centerPoint.x));
+    y = int(round(sin(angle2) * r + centerPoint.y));
+    cv::circle(fft2d, cv::Point(x, y), 35, cv::Scalar(0), -1);
 
-    cv::split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
-    magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
-    cv::Mat magI = planes[0];
+    r = fft2d.cols / 10.5;
+    angle1 = (0.25*M_PI);
+    angle2 = (1.25*M_PI);
+    x = int(round(cos(angle1) * r + centerPoint.x));
+    y = int(round(sin(angle1) * r + centerPoint.y));
+    cv::circle(fft2d, cv::Point(x, y), 25, cv::Scalar(0), -1);
+    x = int(round(cos(angle2) * r + centerPoint.x));
+    y = int(round(sin(angle2) * r + centerPoint.y));
+    cv::circle(fft2d, cv::Point(x, y), 25, cv::Scalar(0), -1);
 
-    magI += cv::Scalar::all(1);                    // switch to logarithmic scale
-    cv::log(magI, magI);
+    vis::showfft2dMagnitude("filter applied to freq mag", fft2d, scale / 2);
 
-    magI = magI(cv::Rect(0, 0, magI.cols & -2, magI.rows & -2));
-    int cx = magI.cols/2;
-    int cy = magI.rows/2;
+    cv::Mat res = vis::ifft2d(fft2d);
+    vis::show("res", res, scale);
 
-    cv::Mat q0(magI, cv::Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
-    cv::Mat q1(magI, cv::Rect(cx, 0, cx, cy));  // Top-Right
-    cv::Mat q2(magI, cv::Rect(0, cy, cx, cy));  // Bottom-Left
-    cv::Mat q3(magI, cv::Rect(cx, cy, cx, cy)); // Bottom-Right
-
-    cv::Mat tmp;                           // swap quadrants (Top-Left with Bottom-Right)
-    q0.copyTo(tmp);
-    q3.copyTo(q0);
-    tmp.copyTo(q3);
-
-    q1.copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
-    q2.copyTo(q1);
-    tmp.copyTo(q2);
-
-    normalize(magI, magI, 0, 1, CV_MINMAX); // Transform the matrix with float values into a
-    // viewable image form (float between values 0 and 1).
-
-    vis::show("Freq spektrum", magI, 0.3);
 
     cv::waitKey(0);
     return 0;
