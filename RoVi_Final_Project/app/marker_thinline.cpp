c386edbab314c0ead2e6d0473a9ca0b36b781667
frederiknoer <frederik.noer15@gmail.com>
2018-12-05 09:44:47 +0100

    enhanced marker finding with angle criteria

--- RoVi_Final_Project/app/marker_thinline.cpp
+++ RoVi_Final_Project/app/marker_thinline.cpp
@@ -6,6 +6,29 @@
 #include <opencv2/opencv_modules.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/core.hpp>
+#include <cmath>
+#include <math.h>
+
+
+double findAngle(cv::Point &p11, cv::Point &p12, cv::Point &p21, cv::Point &p22)
+{
+    /*
+    double m1 = double(abs(p12.y - p11.y)) / double(abs(p12.x - p11.x));   // tan(A)
+    double A = atan(m1) * 180 / M_PI;
+
+    double m2 = double(abs(p22.y - p21.y)) / double(abs(p22.x - p21.x));  // tan(B)
+    double B = atan(m2) * 180 / M_PI;
+
+    std::cout << atan2(p12.y - p11.y, p12.x - p11.x) << std::endl;
+    std::cout << atan2(p22.y - p21.y, p22.x - p21.x) << std::endl;
+
+    std::cout << "A: " << A << "  B: " << B << "   B - A = " << B+A << std::endl;
+*/
+    double A = atan2(p12.y - p11.y, p12.x - p11.x);
+    double B = atan2(p22.y - p21.y, p22.x - p21.x);
+
+    return (fabsf(A-B))*180/M_PI;
+}
 
 double cross(cv::Point v1, cv::Point v2)
 {
@@ -17,15 +40,45 @@ bool getIntersectionPoint(cv::Point &a1, cv::Point &a2, cv::Point &b1, cv::Point
     cv::Point r(a2-a1);
     cv::Point s(b2-b1);
 
+
     if(cross(r,s) == 0)
     {
-        std::cout << "No intersection" << std::endl;
+        //std::cout << "No intersection" << std::endl;
         return false;
     }
 
     double t = cross(b1-a1,s)/cross(r,s);
     intPnt = a1 + t*r;
 
+    std::vector<int> x_list;
+    x_list.push_back(a1.x); x_list.push_back(a2.x);
+    std::vector<int> y_list;
+    y_list.push_back(a1.y); y_list.push_back(a2.y);
+
+    std::sort(x_list.begin(), x_list.end());
+    std::sort(y_list.begin(), y_list.end());
+
+    if ( intPnt.x < x_list[0] || intPnt.x > x_list[1] )
+        return false;
+    if ( intPnt.y < y_list[0] || intPnt.y > y_list[1] )
+        return false;
+
+    x_list.clear(); y_list.clear();
+    x_list.push_back(b1.x); x_list.push_back(b2.x); y_list.push_back(b1.y); y_list.push_back(b2.y);
+
+    std::sort(x_list.begin(), x_list.end());
+    std::sort(y_list.begin(), y_list.end());
+
+    if ( intPnt.x < x_list[0] || intPnt.x > x_list[1] )
+        return false;
+    if ( intPnt.y < y_list[0] || intPnt.y > y_list[1] )
+        return false;
+
+    double tmpAng = findAngle(a1, a2, b1, b2);
+    //std::cout << "temp ang: " << tmpAng << std::endl;
+
+    if (tmpAng < 80 || tmpAng > 100)
+        return false;
     return true;
 }
 
@@ -62,8 +115,10 @@ int main() {
 
     while (true)
     {
-        for(int i = 0; i < 30; i++)
+        for(int i = 18; i < 30; i++)
         {
+            std::cout << "imgae nr: " << i << std::endl;
+
             cv::Mat hls;
             cv::cvtColor(imgSequence[i], hls, cv::COLOR_BGR2HLS);
             cv::Mat planes[3], l_mat, thr1;
@@ -84,8 +139,11 @@ int main() {
 
 
             //Find lines and draw them to a new image:
-            std::vector<cv::Vec4i> lines;
+            std::vector<cv::Vec4i> lines, lines_f;
             HoughLinesP(imgCanny[i], lines, 1, CV_PI/270, 8, 220, 18);
+
+
+            //std::cout << lines.size() << std::endl;
             cv::Point marker;
             std::vector<cv::Point> markers;
             for (int l = 0; l < lines.size() - 1; l++)
@@ -101,25 +159,6 @@ int main() {
                     {
                         cv::circle(imgSequence[i], marker, 8, cv::Scalar(0,255,0), -1);
                         markers.push_back(marker);
-                    }
-
-                }
-            }
-            for (int m = 0; m < markers.size(); m++)
-            {
-                int m_dist = markers[m].x * markers[m].y;
-                for (int mx = m+1; mx < markers.size(); mx++)
-                {
-                    int dist_counter = 0;
-                    int mx_dist = markers[mx].x * markers[mx].y;
-
-                    if (m_dist > mx_dist - 2 && m_dist < mx_dist + 2)
-                        dist_counter++;
-
-                    if (dist_counter < 3)
-                    {
-                        markers.erase(markers.begin() + m);
-                        m--;
                     }
                 }
             }
